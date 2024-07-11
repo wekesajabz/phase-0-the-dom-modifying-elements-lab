5ff91f4dd0bee6ca514450382d0a23df8b4aa5f7
jabez wekesa <wekesajabez6550@gmail.com>
2024-07-11 10:06:27 +0300

    Added the delete DoM methods

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+let mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.parentNode.removeChild(mainElement);
+}
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.textContent = 'Jabez is the champion';
+
+
+
+
+
